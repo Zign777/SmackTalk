000abcb48c6b92758dfaffe1cbd9c1a107b83714
Etai Pinckney <etai.pinckney@gmail.com>
2013-10-25 10:55:35 -0700

    Bluetooth Chat is functional

--- MessageActivity/src/com/smacktalk/main/MessageActivity.java
+++ MessageActivity/src/com/smacktalk/main/MessageActivity.java
@@ -6,19 +6,39 @@ import java.util.Random;
 
 
 
+
+
+
+
 import com.color.speechbubble.R;
 import com.color.speechbubble.R.id;
 import com.color.speechbubble.R.layout;
+import com.smacktalk.utility.BluetoothChatService;
 import com.smacktalk.utility.BroadcastChatService;
 
+import android.app.ActionBar;
+import android.app.Activity;
 import android.app.ListActivity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -30,46 +50,37 @@ public class MessageActivity extends ListActivity {
 	EditText text;
 	static Random rand = new Random();	
 	static String sender;
-	private static final String TAG = "BcastChat";
-	private static final boolean D = true;
+	 // Debugging
+    private static final String TAG = "BluetoothChat";
+    private static final boolean D = true;
 
-	    // Message types sent from the BluetoothChatService Handler
-	public static final int MESSAGE_READ 	= 1;
-	public static final int MESSAGE_WRITE 	= 2;
-	public static final int MESSAGE_TOAST 	= 3;
-	private BroadcastChatService mChatService = null;
-	private StringBuffer mOutStringBuffer;
+    // Message types sent from the BluetoothChatService Handler
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
 
-	    // Key names received from the BroadcastChatService Handler
-	public static final String TOAST = "toast";
+    // Key names received from the BluetoothChatService Handler
+    public static final String DEVICE_NAME = "device_name";
+    public static final String TOAST = "toast";
+
+    // Intent request codes
+    private static final int REQUEST_CONNECT_DEVICE_SECURE = 1;
+    private static final int REQUEST_CONNECT_DEVICE_INSECURE = 2;
+    private static final int REQUEST_ENABLE_BT = 3;
+    // Name of the connected device
+    private String mConnectedDeviceName = null;
+    // String buffer for outgoing messages
+    private StringBuffer mOutStringBuffer;
+    // Local Bluetooth adapter
+    private BluetoothAdapter mBluetoothAdapter = null;
+    // Member object for the chat services
+    private BluetoothChatService mChatService = null;
+
+
 	
-	private final Handler mHandler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-        	
-        	if(D) Log.e(TAG, "[handleMessage !!!!!!!!!!!! ]");
-        	
-            switch (msg.what) {
-            
-	            case MESSAGE_WRITE:
-	            	
-	                byte[] writeBuf = (byte[]) msg.obj;
-	                // construct a string from the buffer
-	                String writeMessage = new String(writeBuf);
-	                addNewMessage(new SmackTalkMessage("Me: "+writeMessage, true));
-	                break;
-	            case MESSAGE_READ:
-	                String readBuf = (String) msg.obj;
-	                addNewMessage(new SmackTalkMessage("You: "+readBuf, false));
-	                break;               
-	            case MESSAGE_TOAST:
-	                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
-	                               Toast.LENGTH_SHORT).show();
-	                break;
-            }
-        }
-    };    
-	
+		
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -85,97 +96,314 @@ public class MessageActivity extends ListActivity {
 		messages.add(new SmackTalkMessage("Hi!"+"\n - Joelle", false));
 		messages.add(new SmackTalkMessage("What's up"+"\n - John", false));
 		
-
 		adapter = new MessageAdapter(this, messages);
 		setListAdapter(adapter);
-		  // Initialize the BluetoothChatService to perform bluetooth connections
-        mChatService = new BroadcastChatService(this, mHandler);
+		
+		mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+	        // If the adapter is null, then Bluetooth is not supported
+	        if (mBluetoothAdapter == null) {
+	            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
+	            finish();
+	            return;
+	        }
 
         // Initialize the buffer for outgoing messages
         mOutStringBuffer = new StringBuffer("");
 	
 	}
-	@Override
-    public synchronized void onResume() {
-        super.onResume();
-        if(D) Log.e(TAG, "+ ON RESUME +");
-        
-        mChatService.start();
-    }
-	public void sendMessage(View v)
-	{
-		String newMessage = text.getText().toString().trim(); 
-		if(D) Log.e(TAG, "[sendMessage]");
-    	
-        // Check that there's actually something to send
-        if (newMessage.length() > 0 ) {
-            // Get the message bytes and tell the BluetoothChatService to write
-            byte[] send = newMessage.getBytes();
-            mChatService.write(send);
+	 @Override
+	    public void onStart() {
+	        super.onStart();
+	        if(D) Log.e(TAG, "++ ON START ++");
 
-            // Reset out string buffer to zero and clear the edit text field
-            mOutStringBuffer.setLength(0);
-            text.setText(mOutStringBuffer);
-            
-        }
-	}
-	private class SendMessage extends AsyncTask<Void, String, String>
-	{
-		@Override
-		protected String doInBackground(Void... params) {
-			try {
-				Thread.sleep(2000); //simulate a network call
-			}catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			this.publishProgress(String.format("%s started writing", sender));
-			try {
-				Thread.sleep(2000); //simulate a network call
-			}catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			this.publishProgress(String.format("%s has entered text", sender));
-			try {
-				Thread.sleep(3000);//simulate a network call
-			}catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-			
-			return Utility.messages[rand.nextInt(Utility.messages.length-1)];
-			
-			
-		}
-		@Override
-		public void onProgressUpdate(String... v) {
-			
-			if(messages.get(messages.size()-1).isStatusMessage)//check wether we have already added a status message
-			{
-				messages.get(messages.size()-1).setMessage(v[0]); //update the status for that
-				adapter.notifyDataSetChanged(); 
-				getListView().setSelection(messages.size()-1);
-			}
-			else{
-				addNewMessage(new SmackTalkMessage(true,v[0])); //add new message, if there is no existing status message
-			}
-		}
-		@Override
-		protected void onPostExecute(String text) {
-			if(messages.get(messages.size()-1).isStatusMessage)//check if there is any status message, now remove it.
-			{
-				messages.remove(messages.size()-1);
-			}
-			
-			addNewMessage(new SmackTalkMessage(text, false)); // add the orignal message from server.
-		}
-		
+	        // If BT is not on, request that it be enabled.
+	        // setupChat() will then be called during onActivityResult
+	        if (!mBluetoothAdapter.isEnabled()) {
+	            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+	            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+	        // Otherwise, setup the chat session
+	        } else {
+	            if (mChatService == null) setupChat();
+	        }
+	    }
 
-	}
+	    @Override
+	    public synchronized void onResume() {
+	        super.onResume();
+	        if(D) Log.e(TAG, "+ ON RESUME +");
+
+	        // Performing this check in onResume() covers the case in which BT was
+	        // not enabled during onStart(), so we were paused to enable it...
+	        // onResume() will be called when ACTION_REQUEST_ENABLE activity returns.
+	        if (mChatService != null) {
+	            // Only if the state is STATE_NONE, do we know that we haven't started already
+	            if (mChatService.getState() == BluetoothChatService.STATE_NONE) {
+	              // Start the Bluetooth chat services
+	              mChatService.start();
+	            }
+	        }
+	    }
+
+	    private void setupChat() {
+	        Log.d(TAG, "setupChat()");
+
+	        // Initialize the BluetoothChatService to perform bluetooth connections
+	        mChatService = new BluetoothChatService(this, mHandler);
+
+	        // Initialize the buffer for outgoing messages
+	        mOutStringBuffer = new StringBuffer("");
+	    }
+
+	    @Override
+	    public synchronized void onPause() {
+	        super.onPause();
+	        if(D) Log.e(TAG, "- ON PAUSE -");
+	    }
+
+	    @Override
+	    public void onStop() {
+	        super.onStop();
+	        if(D) Log.e(TAG, "-- ON STOP --");
+	    }
+
+	    @Override
+	    public void onDestroy() {
+	        super.onDestroy();
+	        // Stop the Bluetooth chat services
+	        if (mChatService != null) mChatService.stop();
+	        if(D) Log.e(TAG, "--- ON DESTROY ---");
+	    }
+
+	    private void ensureDiscoverable() {
+	        if(D) Log.d(TAG, "ensure discoverable");
+	        if (mBluetoothAdapter.getScanMode() !=
+	            BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
+	            Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
+	            discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 300);
+	            startActivity(discoverableIntent);
+	        }
+	    }
+	
 	void addNewMessage(SmackTalkMessage m)
 	{
 		messages.add(m);
 		adapter.notifyDataSetChanged();
 		getListView().setSelection(messages.size()-1);
 	}
+	public void sendMessage(View v){
+		sendMessage(text.getText().toString());
+	}
+    private void sendMessage(String message) {
+        // Check that we're actually connected before trying anything
+        if (mChatService.getState() != BluetoothChatService.STATE_CONNECTED) {
+            //Toast.makeText(this, R.string.not_connected, Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Check that there's actually something to send
+        if (message.length() > 0) {
+            // Get the message bytes and tell the BluetoothChatService to write
+            byte[] send = message.getBytes();
+            mChatService.write(send);
+
+            // Reset out string buffer to zero and clear the edit text field
+            mOutStringBuffer.setLength(0);
+            text.setText(mOutStringBuffer);
+        }
+    }
+
+    // The action listener for the EditText widget, to listen for the return key
+    private TextView.OnEditorActionListener mWriteListener =
+        new TextView.OnEditorActionListener() {
+        public boolean onEditorAction(TextView view, int actionId, KeyEvent event) {
+            // If the action is a key-up event on the return key, send the message
+            if (actionId == EditorInfo.IME_NULL && event.getAction() == KeyEvent.ACTION_UP) {
+                String message = view.getText().toString();
+                sendMessage(message);
+            }
+            if(D) Log.i(TAG, "END onEditorAction");
+            return true;
+        }
+    };
+
+    private final void setStatus(int resId) {
+        //final ActionBar actionBar = getActionBar();
+        //actionBar.setSubtitle(resId);
+    }
+
+    private final void setStatus(CharSequence subTitle) {
+        //final ActionBar actionBar = getActionBar();
+        //actionBar.setSubtitle(subTitle);
+    }
+
+    // The Handler that gets information back from the BluetoothChatService
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case MESSAGE_STATE_CHANGE:
+                if(D) Log.i(TAG, "MESSAGE_STATE_CHANGE: " + msg.arg1);
+                switch (msg.arg1) {
+                case BluetoothChatService.STATE_CONNECTED:
+                    setStatus("Connected to" + mConnectedDeviceName);
+                    //adapter.clear();
+                    break;
+                case BluetoothChatService.STATE_CONNECTING:
+                    setStatus("Connecting");
+                    break;
+                case BluetoothChatService.STATE_LISTEN:
+                case BluetoothChatService.STATE_NONE:
+                    setStatus("Not connected");
+                    break;
+                }
+                break;
+            case MESSAGE_WRITE:
+                byte[] writeBuf = (byte[]) msg.obj;
+                // construct a string from the buffer
+                String writeMessage = new String(writeBuf);
+                addNewMessage(new SmackTalkMessage("Me: "+ writeMessage,true));
+                break;
+            case MESSAGE_READ:
+                byte[] readBuf = (byte[]) msg.obj;
+                // construct a string from the valid bytes in the buffer
+                String readMessage = new String(readBuf, 0, msg.arg1);
+                addNewMessage(new SmackTalkMessage(mConnectedDeviceName+":  " + readMessage,false));
+                break;
+            case MESSAGE_DEVICE_NAME:
+                // save the connected device's name
+                mConnectedDeviceName = msg.getData().getString(DEVICE_NAME);
+                Toast.makeText(getApplicationContext(), "Connected to "
+                               + mConnectedDeviceName, Toast.LENGTH_SHORT).show();
+                break;
+            case MESSAGE_TOAST:
+                Toast.makeText(getApplicationContext(), msg.getData().getString(TOAST),
+                               Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+    };
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(D) Log.d(TAG, "onActivityResult " + resultCode);
+        switch (requestCode) {
+        case REQUEST_CONNECT_DEVICE_SECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, true);
+            }
+            break;
+        case REQUEST_CONNECT_DEVICE_INSECURE:
+            // When DeviceListActivity returns with a device to connect
+            if (resultCode == Activity.RESULT_OK) {
+                connectDevice(data, false);
+            }
+            break;
+        case REQUEST_ENABLE_BT:
+            // When the request to enable Bluetooth returns
+            if (resultCode == Activity.RESULT_OK) {
+                // Bluetooth is now enabled, so set up a chat session
+                setupChat();
+            } else {
+                // User did not enable Bluetooth or an error occurred
+                Log.d(TAG, "BT not enabled");
+                Toast.makeText(this, "Bluetooth was not enabled. Leaving Bluetooth Chat.", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    private void connectDevice(Intent data, boolean secure) {
+        // Get the device MAC address
+        String address = data.getExtras()
+            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+        // Get the BluetoothDevice object
+        BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+        // Attempt to connect to the device
+        mChatService.connect(device, secure);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	System.out.println("Wtf");
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.option_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent serverIntent = null;
+        switch (item.getItemId()) {
+        case R.id.secure_connect_scan:
+            // Launch the DeviceListActivity to see devices and do scan
+            serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_SECURE);
+            return true;
+        case R.id.insecure_connect_scan:
+            // Launch the DeviceListActivity to see devices and do scan
+            serverIntent = new Intent(this, DeviceListActivity.class);
+            startActivityForResult(serverIntent, REQUEST_CONNECT_DEVICE_INSECURE);
+            return true;
+        case R.id.discoverable:
+            // Ensure this device is discoverable by others
+            ensureDiscoverable();
+            return true;
+        }
+        return false;
+    }
 }
+/*
+private class SendMessage extends AsyncTask<Void, String, String>
+{
+	@Override
+	protected String doInBackground(Void... params) {
+		try {
+			Thread.sleep(2000); //simulate a network call
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		this.publishProgress(String.format("%s started writing", sender));
+		try {
+			Thread.sleep(2000); //simulate a network call
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		this.publishProgress(String.format("%s has entered text", sender));
+		try {
+			Thread.sleep(3000);//simulate a network call
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return Utility.messages[rand.nextInt(Utility.messages.length-1)];
+		
+		
+	}
+	@Override
+	public void onProgressUpdate(String... v) {
+		
+		if(messages.get(messages.size()-1).isStatusMessage)//check wether we have already added a status message
+		{
+			messages.get(messages.size()-1).setMessage(v[0]); //update the status for that
+			adapter.notifyDataSetChanged(); 
+			getListView().setSelection(messages.size()-1);
+		}
+		else{
+			addNewMessage(new SmackTalkMessage(true,v[0])); //add new message, if there is no existing status message
+		}
+	}
+	@Override
+	protected void onPostExecute(String text) {
+		if(messages.get(messages.size()-1).isStatusMessage)//check if there is any status message, now remove it.
+		{
+			messages.remove(messages.size()-1);
+		}
+		
+		addNewMessage(new SmackTalkMessage(text, false)); // add the orignal message from server.
+	}
+	
+
+}*/
